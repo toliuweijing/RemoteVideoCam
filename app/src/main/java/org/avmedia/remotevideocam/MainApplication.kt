29f9ba5e147996dbe958b930118e89cc8a082c8c
toliuweijing <waiging.lau@gmail.com>
2024-08-05 17:20:06 +0800

    timber

---
+++ app/src/main/java/org/avmedia/remotevideocam/MainApplication.kt
@@ -0,0 +1,16 @@
+package org.avmedia.remotevideocam
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
